cdabfc67433ebf772eb3b5d7522f5c23017f6c93
Revy <43113218+RevanthNagarajan@users.noreply.github.com>
2021-03-10 12:45:09 +0530

    Initialization

---
+++ src/main/java/com/service/wageometer/WageometerApplication.java
@@ -0,0 +1,13 @@
+package com.service.wageometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WageometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WageometerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/service/wageometer/WageometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.wageometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WageometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
